b7ff2231d319bffc324c49cc324ab5959fc6dc0f
Ángel Perea Arias <44724632+ariasangel@users.noreply.github.com>
2022-12-16 12:40:37 +0100

    Added Cache notation

--- similarProducts/src/main/java/mca/backendTechnicalTest/similarProducts/client/ProductFeignClient.java
+++ similarProducts/src/main/java/mca/backendTechnicalTest/similarProducts/client/ProductFeignClient.java
@@ -1,6 +1,7 @@
 package mca.backendTechnicalTest.similarProducts.client;
 
 import mca.backendTechnicalTest.similarProducts.model.Product;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +13,10 @@ import java.util.List;
 public interface ProductFeignClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/{productId}/similarids")
+    @Cacheable(cacheNames = "similar-ids", key = "#productId")
     List<String> getSimilarIds(@PathVariable("productId") String productId);
 
     @RequestMapping(method = RequestMethod.GET, value = "/{productId}")
+    @Cacheable(cacheNames = "products", key = "#productId")
     Product getProduct(@PathVariable("productId") String productId);
 }
